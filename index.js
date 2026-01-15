e89a16de3676822c0512c1c82981fa274d040b0b
Oxsilaris06 <nicolas.maheux45@gmail.com>
2026-01-15 10:50:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import './polyfills'; // FIX: Polyfills importés EN PREMIER pour garantir le support PeerJS
+import './polyfills'; // OBLIGATOIRE : Doit être la toute première ligne
 
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
