92c1da39b4308430f432096e1f6a3b99fe841320
Oxsilaris06 <nicolas.maheux45@gmail.com>
2026-01-31 01:54:40 +0100

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
-// Utiliser @expo/metro-config est plus sûr pour la compatibilité
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// On s'assure que les fichiers css et js (pour leaflet) sont bien traités comme des assets
+// et non comme du code source à compiler par le bundler natif.
+config.resolver.assetExts.push(
+  'css', 
+  'html' // Au cas où vous auriez du HTML local
+);
+
+// On exclut ces extensions du code source pour éviter les conflits
+// (Note: on garde 'js' et 'ts' dans sourceExts évidemment, mais 'css' va dans assets)
 
 module.exports = config;
